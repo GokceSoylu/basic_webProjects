a3dbfcefcfa40df87c923334e9fbab532d366e97
NecmiyeSoylu <necmiyesoyla@gmail.com>
2024-01-18 02:01:02 +0300

    -

--- Todo List/javascript/addTodo.js
+++ Todo List/javascript/addTodo.js
@@ -1,10 +1,10 @@
 const add_button=document.querySelector("#add_todo");
 const add_text=document.querySelector("#add_text");
-let index="a";
+let index=0;
 add_button.addEventListener("click",event=>
 {
     let val=add_text.value;
     localStorage.setItem(index++,val);
-    console.log(val);
+    console.log(localStorage.getItem());//okay
 })
 
--- Todo List/javascript/new_todo.js
+++ Todo List/javascript/new_todo.js
@@ -1,16 +1,17 @@
 const add_todo_new=document.querySelector("#add_todo");
 const todo_area=document.querySelector("#todo_ul");
-let index_2="a";
+let index_2=0;
 add_todo_new.addEventListener("click",event=>
 {
     
     todo_new=document.createElement("li");
 
     todo_h3=document.createElement("h3");
-    todo_h3.innerHTML=localStorage.getItem(++index_2);
+    let x=localStorage.getItem(index_2++);
+    todo_h3.innerHTML=x;
+    console.log("heyyo "+x);
     todo_img=document.createElement("img");
     todo_img.src="/Users/necmiyesoyla/basic_webProjects/Todo List/svgs/cross.svg";
-    
     
     
     todo_new.appendChild(todo_h3);
